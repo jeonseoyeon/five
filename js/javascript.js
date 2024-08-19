3c55801a73b46b46bd84b2fab9ecc2fce46750fb
전서연 <djc07112@naver.com>
2024-08-19 16:49:47 +0900

    0819

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+"use strict";
+// 검색창
+function showSearch() {
+  //document.querySelector("#search_form").style.top = "0";
+  document.querySelector("#search_form").style.opacity = "1";
+  document.querySelector("#search_form").style.height = "280px";
+}
+
+function hideSearch() {
+  //document.querySelector("#search_form").style.top = "-999px";
+  document.querySelector("#search_form").style.opacity = "1";
+  document.querySelector("#search_form").style.height = "0px";
+}
