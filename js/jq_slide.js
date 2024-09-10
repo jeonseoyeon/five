fbe35e2f582a6db8bd79a52d5b3fffaed6ba4ec9
전서연 <djc07112@naver.com>
2024-09-10 13:16:33 +0900

    sect2_slide중

--- js/jq_slide.js
+++ js/jq_slide.js
@@ -1,14 +1,24 @@
 $(function () {
   //   메인비주얼;
-  $(".slide").slick({
+  // $(".slide").slick({
+  //   infinite: true,
+  //   slidesToShow: 1, //한번에 보여줄 슬라이드 아이템 개수
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 5000,
+  //   arrows: false,
+  // });
+
+  // 베스트도서
+  $(".slide2").slick({
     infinite: true,
-    slidesToShow: 1, //한번에 보여줄 슬라이드 아이템 개수
+    slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    arrows: false,
+    prevArrow: $(".btn0"),
+    nextArrow: $(".btn1"),
   });
-  //   신규도서
+
+  // 신규도서
   $(".sect3_innercon").slick({
     dots: true,
     infinite: true,
@@ -19,10 +29,19 @@ $(function () {
     autoplaySpeed: 1500,
     responsive: [
       {
+        breakpoint: 1336,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -31,7 +50,7 @@ $(function () {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
         },
       },
       {
